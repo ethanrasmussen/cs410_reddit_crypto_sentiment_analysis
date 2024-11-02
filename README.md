b60d936961e8c07708b3f0f25b2fc3f733ced59e
Ethan Rasmussen <ethanmr3@illinois.edu>
2024-11-01 19:15:14 -0500

    Start reddit stuff

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cs410_reddit_crypto_sentiment_analysis
 Reddit Post Sentiment Analysis for Crypto-market Trend Prediction
+
+**Scratch notes:**
+We'll likely want these main components:
+ - Reddit post grabber/processing functions
+ - Sentiment analysis tools/funcs (via NLTK?)
+ - Functions for grabbing real-time / real-market data (yfinance would be useful here)
+ - Graphs/visualizations (plotly, matplotlib, etc.)
+ - (Purely optional if we have time) Basic front-end to have up-to-date display & easy for graders/TAs to see
--- src/reddit_interactions.py
+++ src/reddit_interactions.py
@@ -1,2 +1,30 @@
 import requests
 
+
+def get_subreddit_feed_json(subreddit_name:str):
+    return requests.get(f"https://www.reddit.com/r/{subreddit_name}.json").json()
+
+def get_post_json(subreddit_name:str, post_id:str):
+    return requests.get(f"https://www.reddit.com/r/{subreddit_name}/comments/{post_id}/.json").json()
+
+
+class RedditPost:
+    def __init__(self, id:str, title:str, text_content:str, subreddit:str, author:str) -> None:
+        self.id = id
+        self.title = title
+        self.content = text_content
+        self.subreddit = subreddit
+        self.author = author
+
+def get_post_from_id(subreddit_name:str, post_id:str):
+    post_dict = get_post_json(subreddit_name=subreddit_name, post_id=post_id)
+    return RedditPost(
+        id=None,
+        title=None,
+        text_content=None,
+        subreddit=None,
+        author=None
+    )
+
+def get_posts_from_subreddit(subreddit_name:str, after_timestamp = None):
+    pass
